adad9df3fe4f32bfc6c66ca78878bab3dcce79b0
Bohdan Khvorostovskyi <pevbog@gmail.com>
2022-03-05 09:33:02 -0500

    deploy to heroku

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
@@ -15,6 +16,15 @@ app.use(express.urlencoded({ limit: '2mb', extended: false }))
 // Router
 app.use('/api/links', require('./routes/linkRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
+
+// Serv frontend
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
+} else {
+    app.get('/', (req, res) => res.send('Please set to production'))
+}
 
 // Add error handler
 app.use(errorHandler)
